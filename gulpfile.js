93b96ae1832d69af6de66c498a442193ba6ad885
テラ <terakawayusuke@gmail.com>
2025-05-02 21:51:42 +0900

    browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ import gulp from "gulp";
 import gulpSass from "gulp-sass";
 import * as dartSass from "sass";
 import { deleteSync } from "del";
+import browserSync from "browser-sync";
 
 const sass = gulpSass(dartSass);
 
@@ -31,6 +32,27 @@ const copyImg = () =>
     .src("src/**/*.{ico,gif,jpg,jpeg,png,svg,webp,avif,webmanifest}")
     .pipe(gulp.dest("dist"));
 
+const browserInit = (done) => {
+  browserSync.init({ server: { baseDir: "dist" }, port: 3000 });
+  done();
+};
+const browserReload = (done) => {
+  browserSync.reload();
+  done();
+};
+const watchTask = () => {
+  gulp.watch(
+    "src/**/*.{ico,gif,jpg,jpeg,png,svg,webp,avif,webmanifest}",
+    gulp.series(copyImg, browserReload)
+  );
+  gulp.watch(
+    "src/assets/sass/**/*.scss",
+    gulp.series(compileSass, browserReload)
+  );
+  gulp.watch("src/**/*.html", gulp.series(copyHTML, browserReload));
+};
+
+export const watch = gulp.series(browserInit, watchTask);
 export const build = gulp.series(gulp.parallel(compileSass, copyHTML, copyImg));
-export const main = gulp.series(clean, build);
+export const main = gulp.series(clean, build, watch);
 export default main;
